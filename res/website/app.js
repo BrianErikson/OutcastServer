1a3a49ea395f105ef4365adf63fa45fe9afb51a5
BrianErikson <briankerikson@gmail.com>
2016-04-17 16:33:16 +0200

    using custom url validator

--- res/website/app.js
+++ res/website/app.js
@@ -1,5 +1,19 @@
 var outcastApp = angular.module('OutcastApp', []);
 
+// URL regex expression taken from https://mathiasbynens.be/demo/url-regex. @diegoperini's version
+var urlExpression = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
+outcastApp.directive('url', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$validators.url = function(modelValue, viewValue) {
+                return urlExpression.test(viewValue);
+            };
+        }
+    };
+});
+
 outcastApp.controller('MasterCtrl', function ($scope) {
     $scope.toggleNewFeed = function() {
         $("#newFeedDiv").toggle(true);
@@ -10,7 +24,7 @@ outcastApp.controller('NewFeedCtrl', function ($scope) {
     $scope.feed = {title: '', location: ''};
     $scope.submitNewFeed = function() {
         if ($scope.feed.title && $scope.feed.location) {
-            
+
         }
         else {
 
