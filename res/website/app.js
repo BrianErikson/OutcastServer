20580b328bad653cafd5008ea7b30e054397338f
BrianErikson <briankerikson@gmail.com>
2016-04-17 13:13:44 +0200

    db results now show up in website table

--- res/website/app.js
+++ res/website/app.js
@@ -17,8 +17,7 @@ outcastApp.controller('NewFeedCtrl', function ($scope) {
 outcastApp.controller('RssFeedCtrl', function ($scope, $http) {
     $scope.feeds = [{title: "", location: ""}];
     $http.get("/feeds").then(function successCallback(response) {
-        // this callback will be called asynchronously
-        // when the response is available
+        $scope.feeds = response.data;
     }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
